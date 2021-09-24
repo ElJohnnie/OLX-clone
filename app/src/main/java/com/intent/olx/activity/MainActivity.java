0d2237174261a15ec7fef23cb084cf8d49a66036
ElJohnnie <johnfollmer1995@gmail.com>
2021-09-23 21:50:19 -0300

    tela de login ok com firebase

---
+++ app/src/main/java/com/intent/olx/activity/MainActivity.java
@@ -0,0 +1,134 @@
+package com.intent.olx.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.intent.olx.R;
+import com.intent.olx.helper.ConfiguracaoFirebase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoAcessar;
+    private EditText campoEmail, campoSenha;
+    private Switch tipoAcesso;
+
+    private FirebaseAuth autenticaco;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //inicializando os componentes de login
+        inicializarComponentes();
+        autenticaco = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        //ao clicar em acessar, vai existir as verificações
+        botaoAcessar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String email = campoEmail.getText().toString();
+                String senha = campoSenha.getText().toString();
+
+                if( !email.isEmpty()){
+                    if( !senha.isEmpty()){
+                        //verificar o estado do switch
+                        if( tipoAcesso.isChecked() ){
+                            //cadastro
+                            autenticaco.createUserWithEmailAndPassword(
+                                    email,senha
+                            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(
+                                                MainActivity.this,
+                                                "Cadastro realizado com sucesso.",
+                                                Toast.LENGTH_LONG)
+                                                .show();
+                                        //Direcionar para a tela principal do App
+
+                                    }else{
+                                        String erroExcecao = "";
+                                        try {
+                                            throw task.getException();
+                                        }catch (FirebaseAuthWeakPasswordException e){
+                                            erroExcecao = "Digite uma senha mais forte";
+                                        }catch (FirebaseAuthInvalidCredentialsException e){
+                                            erroExcecao = "Por favor, digite um E-mail válido";
+                                        }catch (FirebaseAuthUserCollisionException e){
+                                            erroExcecao = "Está conta já foi cadastrada";
+                                        }catch (Exception e){
+                                            erroExcecao = "ao cadastrar usuário: " + e.getMessage();
+                                            e.printStackTrace();
+                                        }
+                                        Toast.makeText(
+                                                MainActivity.this,
+                                                "Erro: " + erroExcecao,
+                                                Toast.LENGTH_LONG)
+                                                .show();
+                                    }
+                                }
+                            });
+                        }else{
+                            //login
+                            autenticaco.signInWithEmailAndPassword(
+                                    email,senha
+                            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(
+                                                MainActivity.this,
+                                                "Logado com sucesso.",
+                                                Toast.LENGTH_LONG)
+                                                .show();
+                                    }else{
+                                        Toast.makeText(
+                                                MainActivity.this,
+                                                "Erro ao fazer login." + task.getException(),
+                                                Toast.LENGTH_LONG)
+                                                .show();
+                                    }
+                                }
+                            });
+                        }
+                    }else{
+                        Toast.makeText(
+                                MainActivity.this,
+                                "Preencha a senha.",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                }else{
+                    Toast.makeText(
+                            MainActivity.this,
+                            "Preencha o E-Mail.",
+                            Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+        });
+    }
+
+    private void inicializarComponentes(){
+        campoEmail = findViewById(R.id.editCadastroEmail);
+        campoSenha = findViewById(R.id.editCadastroSenha);
+        tipoAcesso = findViewById(R.id.switchAcesso);
+        botaoAcessar = findViewById(R.id.buttonAcesso);
+    }
+}
---
+++ app/src/main/java/com/intent/olx/helper/ConfiguracaoFirebase.java
@@ -0,0 +1,36 @@
+package com.intent.olx.helper;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class ConfiguracaoFirebase {
+    private static DatabaseReference referenciaFirebase;
+    private static FirebaseAuth referenciaAutenticacao;
+    private static StorageReference referenciaStorage;
+
+    //retona referencia do database
+    public static DatabaseReference getFirebase(){
+        if(referenciaFirebase == null){
+            referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+        }
+        return referenciaFirebase;
+    }
+    //retorna a instancia do FirebaseAuth
+    public static FirebaseAuth getFirebaseAutenticacao(){
+        if (referenciaAutenticacao == null){
+            referenciaAutenticacao = FirebaseAuth.getInstance();
+        }
+        return referenciaAutenticacao;
+    }
+
+    public static StorageReference getFirebaseStore(){
+        if(referenciaStorage == null){
+            referenciaStorage = FirebaseStorage.getInstance().getReference();
+        }
+        return referenciaStorage;
+    }
+}
+
